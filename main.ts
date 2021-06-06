c09ea7c0c0b881d24ffdce967934e9669bedc9c8
jyothircool <85424481+jyothircool@users.noreply.github.com>
2021-06-06 11:42:48 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+radio.onReceivedString(function (receivedString) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    led.plotBarGraph(
+    Math.map(signal, -95, -42, 0, 9),
+    9
+    )
+})
+let signal = 0
+radio.setGroup(1)
+radio.setTransmitPower(2)
 basic.forever(function () {
-	
+    radio.sendString("2")
+    basic.pause(200)
 })
