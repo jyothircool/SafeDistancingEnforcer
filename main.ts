468def34dc521f552a3e87d98a70bcb9c62bbb74
jyothircool <85424481+jyothircool@users.noreply.github.com>
2021-08-07 14:53:40 +0800

    Show icon

--- main.ts
+++ main.ts
@@ -4,10 +4,16 @@ radio.onReceivedString(function (receivedString) {
     Math.map(signal, -95, -42, 0, 9),
     9
     )
+    if (signal >= -73 && signal <= -35) {
+        basic.showIcon(IconNames.No)
+        music.playMelody("C5 B C5 B C5 B C5 B ", 500)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
 })
 let signal = 0
 radio.setGroup(1)
-radio.setTransmitPower(2)
+radio.setTransmitPower(4)
 basic.forever(function () {
     radio.sendString("2")
     basic.pause(200)
