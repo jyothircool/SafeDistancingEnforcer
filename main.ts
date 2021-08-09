3cbe35356a42338207292a6fa28a1ae06f938f42
jyothircool <85424481+jyothircool@users.noreply.github.com>
2021-08-09 15:17:24 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,14 +6,22 @@ radio.onReceivedString(function (receivedString) {
     )
     if (signal >= -73 && signal <= -35) {
         basic.showIcon(IconNames.No)
-        music.playMelody("C5 B C5 B C5 B C5 B ", 500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.No)
+        music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
     } else {
         basic.showIcon(IconNames.Yes)
     }
 })
 let signal = 0
 radio.setGroup(1)
-radio.setTransmitPower(4)
+radio.setTransmitPower(2)
 basic.forever(function () {
     radio.sendString("2")
     basic.pause(200)
