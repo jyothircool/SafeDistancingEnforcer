afad534e3edbd62fdea24580f70a7abf8f7e3e8b
jyothircool <85424481+jyothircool@users.noreply.github.com>
2021-11-11 16:21:17 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,52 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("ON")
+    onOff = 1
+})
 radio.onReceivedString(function (receivedString) {
-    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-    led.plotBarGraph(
-    Math.map(signal, -95, -42, 0, 9),
-    9
-    )
-    if (signal >= -63 && signal <= -25) {
-        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
-        basic.showIcon(IconNames.No)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showIcon(IconNames.No)
-    } else {
-        basic.showIcon(IconNames.Yes)
+    if (onOff == 1) {
+        signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+        led.plotBarGraph(
+        Math.map(signal, -95, -42, 0, 9),
+        9
+        )
+        if (signal >= -63 && signal <= -25) {
+            music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+            basic.showIcon(IconNames.No)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showIcon(IconNames.No)
+        } else {
+            basic.showIcon(IconNames.Yes)
+        }
+        if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -30) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("OFF")
+    onOff = 0
+})
 let signal = 0
+let onOff = 0
 radio.setGroup(1)
-radio.setTransmitPower(2)
 basic.forever(function () {
-    radio.sendString("2")
-    basic.pause(200)
+    if (onOff == 1) {
+        radio.setTransmitPower(2)
+        radio.sendString("2")
+        basic.pause(200)
+    } else {
+        radio.setTransmitPower(0)
+    }
 })
